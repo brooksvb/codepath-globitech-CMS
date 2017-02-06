ee62aaf8198e5b254db37d38e74afa76b9abdf79
Brooks Van Buren <woofiewilly@gmail.com>
2017-02-05 21:02:02 -0500

    Fixed has_length, implemented regexps for validation

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -10,38 +10,44 @@
   function has_length($value, $options=array()) {
     // TODO
     $len = strlen($value);
-    if ($len < isset($options['min']) ? $options['min'] : 0||
-    $len > isset($options['max']) ? $options['max'] : 255) {
+    if ($len < (isset($options['min']) ? $options['min'] : 0) ||
+    $len > (isset($options['max']) ? $options['max'] : 255) ) {
       return false;
-    } else return true;
+    } else {
+      return true;
+    }
 
   }
 
   function username_exists($db, $username) {
-    return false;
-    $sql = 'SELECT EXISTS(SELECT * FROM users WHERE username ='.$username.') AS "check"';
+    $sql = 'SELECT EXISTS(SELECT * FROM users WHERE username ="'.$username.'") AS "check";';
     $result = db_query($db, $sql);
     if ($result !== false) {
-      return db_fetch_assoc($result)['check']; // Fetch first row from result, access column 'check'
+      $ret = db_fetch_assoc($result)['check']; // Fetch first row from result, access column 'check'
+      db_free_result($result); // Free the returned object
+      return $ret;
     }
   }
 
   // has_valid_email_format('test@test.com')
   function validate_email($value) {
     // TODO
-    //$regexp = '\/A[A-Za-z]+@[A-Za-z]+.[A-Za-z]\\';
+    $regexp = '/\A[A-Za-z0-9\_\.]+\@[A-Za-z0-9\_\.]+\.[A-Za-z]+\Z/'; // Reg exp for a proper email format
 
+    return preg_match($regexp, $value);
     // strpos() returns position of substring inside value, or false if not found
     // For this reason, we must strict compare to false, because index 0 is falsey
-     return !(strpos($value, '@') !== false);
+    // return (strpos($value, '@') !== false);
   }
 
   function validate_name($value) {
-    return true;
+    $regexp = '/\A[A-Za-z\s\-\,\.\']+\Z/'; // Allowed characters
+    return preg_match($regexp, $value);
   }
 
   function validate_username($value) {
-    return true;
+    $regexp = '/\A[A-Za-z0-9\_]+\Z/'; // alphanumeric and underscores
+    return preg_match($regexp, $value);
   }
 
 ?>
